7de37dd25a8d0897c29188b58acfe3a22beb5853
sergeyzoloto <sergiozoloto@gmail.com>
2024-06-23 17:57:33 +0200

    refactor PhotoController to use HashMap and add CRUD operations

--- demo/src/main/java/com/example/demo/PhotoController.java
+++ demo/src/main/java/com/example/demo/PhotoController.java
@@ -1,29 +1,70 @@
 package com.example.demo;
 
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collection;
+
 
 @RestController
 public class PhotoController {
 
-  private List<Photo> db = List.of(
-    new Photo("1", "photo1.jpg"), 
-    new Photo("2", "photo2.jpg")
-  );
+  private Map<String, Photo> db = new HashMap<>();
+  {
+    db.put("1", new Photo("1", "photo1.jpg"));
+    db.put("2", new Photo("2", "photo2.jpg"));
+  }
+  
 
   @GetMapping("/photos")
-  public List<Photo> getPhotos() {
-    return db;
+  public Collection<Photo> getPhotos() {
+    return db.values();
   }
 
   @GetMapping("/photos/{id}")
   public Photo getPhoto(@PathVariable String id) {
-    return db.stream()
-      .filter(photo -> photo.getId().equals(id))
-      .findFirst()
-      .orElse(null);
+    Photo photo = db.get(id);
+    if (photo == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, 
+      "Photo not found with id " + id);
+    }
+    return photo;
+  }
+
+  @DeleteMapping("/photos/{id}")
+  public void deletePhoto(@PathVariable String id) {
+    Photo photo = db.remove(id);
+    if (photo == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, 
+      "Photo not found with id " + id);
+    }
+  }
+
+  @PostMapping("/photos")
+  public Photo addPhoto(Photo photo) {
+    photo.setId(UUID.randomUUID().toString());
+    db.put(photo.getId(), photo);
+    return photo;
+  }
+
+  @PutMapping("/photos/{id}")
+  public Photo updatePhoto(@PathVariable String id, Photo photo) {
+    if (!db.containsKey(id)) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, 
+      "Photo not found with id " + id);
+    }
+    photo.setId(id);
+    db.put(id, photo);
+    return photo;
   }
   
 
