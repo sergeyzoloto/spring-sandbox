3e4c945c4ff76855e5183440b10946999485ec63
sergeyzoloto <sergiozoloto@gmail.com>
2024-06-24 21:01:26 +0200

    Add validation and file upload functionality to PhotoController

--- demo/src/main/java/com/example/demo/Photo.java
+++ demo/src/main/java/com/example/demo/Photo.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Photo {
 
   private String id;
-
+  
+  // This annotation is used to validate that 
+  @NotEmpty // the field is not null or empty
   private String filename;
+
+  private byte[] data;
 
   public Photo() {
   }
@@ -30,6 +36,14 @@ public class Photo {
 
   public void setFilename(String filename) {
     this.filename = filename;
+  }
+
+  public byte[] getData() {
+    return data;
+  }
+
+  public void setData(byte[] data) {
+    this.data = data;
   }
 
   
--- demo/src/main/java/com/example/demo/PhotoController.java
+++ demo/src/main/java/com/example/demo/PhotoController.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+
+import jakarta.validation.Valid;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.io.IOException;
 import java.util.Collection;
 
 
@@ -31,7 +37,7 @@ public class PhotoController {
   }
 
   @GetMapping("/photos/{id}")
-  public Photo getPhoto(@PathVariable String id) {
+  public Photo getOne(@PathVariable String id) {
     Photo photo = db.get(id);
     if (photo == null) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, 
@@ -41,7 +47,7 @@ public class PhotoController {
   }
 
   @DeleteMapping("/photos/{id}")
-  public void deletePhoto(@PathVariable String id) {
+  public void deleteOne(@PathVariable String id) {
     Photo photo = db.remove(id);
     if (photo == null) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, 
@@ -49,15 +55,23 @@ public class PhotoController {
     }
   }
 
+  // @Valid is used to validate the request body and @RequestBody
+  // is used to bind the request body to the method parameter.
+  // @RequestPart is used to bind the request part to the method parameter.
+  // MultipartFile is used to represent a file part in a multipart request.
+  // The method returns the created photo object.
   @PostMapping("/photos")
-  public Photo addPhoto(Photo photo) {
+  public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+    Photo photo = new Photo();
     photo.setId(UUID.randomUUID().toString());
+    photo.setFilename(file.getOriginalFilename());
+    photo.setData(file.getBytes());
     db.put(photo.getId(), photo);
     return photo;
   }
 
   @PutMapping("/photos/{id}")
-  public Photo updatePhoto(@PathVariable String id, Photo photo) {
+  public Photo updateOne(@PathVariable String id, Photo photo) {
     if (!db.containsKey(id)) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, 
       "Photo not found with id " + id);
